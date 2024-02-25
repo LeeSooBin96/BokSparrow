808bb67a8d58b4f6063a1154de81be928b61ab39
LeeSooBin96 <a9622880@naver.com>
2024-02-25 22:42:51 +0900

    friend

--- boknest/boknest.cpp
+++ boknest/boknest.cpp
@@ -24,6 +24,10 @@ std::vector<UserInfo> userList; //접속한 유저 정보 저장
 void UserLogin(SOCKET&,std::string);
 //친구 정보 전송(전송할 소켓)
 void SendFriendList(SOCKET&,std::string);
+//친구 추가
+void AddFriend(SOCKET&,std::string);
+//친구 찾기
+void FindFriend(SOCKET&,std::string);
 /*유저 정보 관련 구조체, 벡터, 함수  ----end*/
 
 int main()
@@ -52,7 +56,7 @@ unsigned WINAPI HandlingClient(void* arg)
     SOCKET clntSock=*((SOCKET*)arg);
     //메시지 수신시 사용하는 것들
     char buffer[BUF_SIZE]={0};
-    std::string bufString;
+    std::string bufString, nickname;
     int recvLen, strLen; //수신메시지 길이, 문자열 길이
 
     while(recv(clntSock,buffer,BUF_SIZE,0)>0) //수신상태 유지
@@ -67,7 +71,7 @@ unsigned WINAPI HandlingClient(void* arg)
             if(recv(clntSock,buffer,BUF_SIZE,0)<=0) return 0; //수신오류나면 스레드 종료
             bufString.append(buffer); recvLen=bufString.size();
         }
-        std::cout<<"전달된 메시지: "<<bufString<<std::endl;
+        std::cout<<"전달된 메시지: "<<bufString<<std::endl; //임시
         std::string msg=split(bufString,':')[1];
         if(msg=="Nick")
         {
@@ -75,17 +79,27 @@ unsigned WINAPI HandlingClient(void* arg)
         }
         else if(msg=="FriendList")
         {
-            std::cout<<"친구 정보 어떻게 줄까 \n";
+            nickname=split(bufString,':')[2];
             SendFriendList(clntSock,split(bufString,':')[2]);
+        }
+        else if(msg=="Add")
+        {
+            //친구 추가
+            AddFriend(clntSock,split(bufString,':')[2]);
+        }
+        else if(msg=="Find")
+        {
+            //친구 찾기
+            FindFriend(clntSock,split(bufString,':')[2]);
         }
 
         memset(buffer,0,BUF_SIZE); //버퍼 초기화
     }
-    std::cout<<"접속이 종료되었습니다. \n";
+    std::cout<<"접속이 종료되었습니다 : "<<nickname<<std::endl;
     //접속 종료 시 유저 접속 상태 변화
-    for(auto mem: userList)
+    for(auto& mem: userList) //종료된 이후 플래그 조정
     {
-        if(mem.sock==clntSock)
+        if(mem.nick==nickname) //소켓이 아닌 닉네임으로 구분하는게 정확
         {
             mem.conflag=false;
         }
@@ -129,7 +143,7 @@ void SendFriendList(SOCKET& sock,std::string nick)
     char tmp[BUF_SIZE]={0};
     std::string msg;
     //우선 해당하는 유저 정보 구조체 찾아야함
-    for(auto mem: userList)
+    for(auto& mem: userList) //값 변경할때는 참조로!!! --잊지 말자 ㅠㅠ
     {
         if(mem.nick==nick)
         {
@@ -144,7 +158,7 @@ void SendFriendList(SOCKET& sock,std::string nick)
             {
                 itoa(mem.friendList.size(),tmp,10); //친구수
                 msg.append(tmp);
-                msg=":"+msg+":";
+                msg=":friend:"+msg+":";
                 for(int i=0;i<mem.friendList.size();i++)
                 {
                     msg.append(mem.friendList[i]+":"); //닉네임 추가
@@ -164,14 +178,84 @@ void SendFriendList(SOCKET& sock,std::string nick)
                         }
                     }
                 }
-                std::cout<<msg<<std::endl;
-                //복잡해서 친구 기능 넣고 테스트하면서 돌리자
+                msg.pop_back();
+                //:친구수:친구이름:상태... 이제 길이 추가하자
+                memset(tmp,0,BUF_SIZE);
+                itoa(msg.size(),tmp,10);
+                msg=tmp+msg;
+                send(sock,msg.c_str(),msg.size(),0);
             }
             else
             {
-                send(sock,"5:none",7,0);
+                send(sock,"12:friend:none",15,0);
             }
             break;
         }
     }
+}
+//친구 추가
+void AddFriend(SOCKET& sock,std::string nick)
+{
+    for(auto& mem: userList)
+    {
+        if(mem.sock==sock)
+        {
+            bool ck=false;
+            for(auto m: mem.friendList)
+            {
+                if(m==nick) //기존 친구 리스트에 있으면
+                {
+                    ck=true; break;
+                }
+            }
+            if(!ck) //친구리스트에 없으면
+            { //사실 바로 추가가 아니라 유저 목록에 있는지 확인하고 상대방것에도 나를 추가해야함
+                //작업이 복잡하군
+                std::string msg;
+                for(auto& m: userList)
+                {
+                    if(m.nick==nick)//유저 목록에 있다면
+                    {
+                        m.friendList.push_back(mem.nick);//친구것에도 추가해야함
+                        mem.friendList.push_back(nick); //이게 왜 안먹히지--해결 하...
+                        send(sock,"12:add:Success",15,0);
+                        return;
+                    }
+                }
+            }
+            break;
+        }
+    }
+    send(sock,"11:add:Failed",14,0);
+}
+//친구 찾기
+void FindFriend(SOCKET& sock,std::string keyword)
+{
+    char buffer[BUF_SIZE]={0};
+    std::string msg=":";
+    int count=0;
+    for(auto mem: userList)
+    {   //자기자신 제외하고
+        if(sock!=mem.sock&&mem.nick.find(keyword)<mem.nick.size()) //키워드 포함하는 닉네임 있으면
+        {
+            count++;
+            msg.append(mem.nick+":");
+            if(mem.conflag)
+            {
+                msg.append("T:");
+            }
+            else
+            {
+                msg.append("F:");
+            }
+        }
+    }
+    msg.pop_back();
+    if(count==0) send(sock,"10:find:none",13,0);
+    itoa(count,buffer,10);
+    msg=buffer+msg; msg=":find:"+msg;
+    itoa(msg.size(),buffer,10);
+    msg=buffer+msg;
+    send(sock,msg.c_str(),msg.size(),0);
+    std::cout<<msg<<std::endl;
 }
--- boksparrow/boksparrow.cpp
+++ boksparrow/boksparrow.cpp
@@ -1,6 +1,8 @@
 //복스패로우 클라이언트
+#include <conio.h>
 #include <string>
 #include <vector>
+#include <process.h>
 
 #include "clientbase.h"
 
@@ -8,14 +10,28 @@ const char* SERVER_IP="127.0.0.1";
 const char* PORT_NUM="91016";
 const unsigned int BUF_SIZE=1024;
 
+ClientBase clnt(SERVER_IP,PORT_NUM); //이걸 전역으로 빼면?
+
 std::string nickName; //닉네임을 저장할 문자열
+unsigned int friendNum=0; //친구 수 저장
+unsigned int posforF=0; //친구창 커서 위치
 
 //문자열 스플릿(split)
 std::vector<std::string> split(std::string,const char);
+//수신 오류 --서버의 종료
+void ReceiveError(void) {std::cout<<"서버 종료 \n"; exit(1);};
+//메시지 수신 스레드
+unsigned WINAPI ReceiveMSG(void* arg);
 //서버 접속(로그인) 프로세스
-void LoginProcess(ClientBase&);
-//친구 정보 요청 및 목록 출력
-void RequestFriendList(ClientBase&);
+bool LoginProcess();
+//친구 정보 요청
+void RequestFriendList();
+//친구 목록 출력
+void PrintFriendList(std::vector<std::string>);
+//친구 추가
+void AddorFindMyFriend(std::string);
+//친구 찾기
+void PrintFoundFriend(std::vector<std::string>);
 
 int main()
 {
@@ -23,13 +39,67 @@ int main()
     char buffer[BUF_SIZE]={0}; 
     std::string msg;
     //서버에 접속
-    ClientBase clnt(SERVER_IP,PORT_NUM);
+    // ClientBase clnt(SERVER_IP,PORT_NUM);
     //로그인 --닉네임 등록
-    LoginProcess(clnt);
+    while(!LoginProcess()); 
+    //수신 스레드 생성
+    HANDLE hRecvThread;
+    hRecvThread=(HANDLE)_beginthreadex(NULL,0,ReceiveMSG,nullptr,0,NULL);
+
+    //친구 목록 창 메뉴 선택
+    while(true)
+    {
+        int input;
+        bool quit=false;
+        RequestFriendList();
+        while(!quit)
+        {
+            input=getch();
+            switch (input)
+            {
+            case 72:  
+            case 119:
+                if(posforF!=0) 
+                {
+                    posforF--;
+                    RequestFriendList();
+                }
+                break;
+            case 80: 
+            case 115:
+                if(posforF!=1+friendNum)
+                {
+                    posforF++;
+                    RequestFriendList();
+                }
+                break;
+            case 13:
+                quit=true;
+                break;
+            default:
+                continue;
+            }
+        }
+        if(posforF<friendNum)
+        {
+            //친구 선택한것
+            std::cout<<"아마 친구를 선택하지 않았을까? \n";
+        }
+        else if(posforF==friendNum)
+        {
+            //친구 찾기 --닉네임 입력-서버 송신-수신-결과 출력
+            AddorFindMyFriend("Find");
+        }
+        else if(posforF==friendNum+1)
+        {
+            //친구 추가 
+            AddorFindMyFriend("Add");
+        }
+        Sleep(2000); //임시
+    }
     
-
-
-    Sleep(20000); //임시
+    //스레드 종료하면 메인 종료되어야함
+    WaitForSingleObject(hRecvThread,INFINITE);
     return 0;
 }
 std::vector<std::string> split(std::string string,const char D)
@@ -43,8 +113,60 @@ std::vector<std::string> split(std::string string,const char D)
     }
     return result;
 }
+//메시지 수신 스레드 --왜 동작안하지?
+unsigned WINAPI ReceiveMSG(void* arg)
+{
+    // SOCKET tcpSock=*((SOCKET*)arg); //이것만하면 오류나네 --전역으로 뺌;; 스택에 있는거라 못읽는듯
+    // //버퍼로 사용할 문자열
+    char buffer[BUF_SIZE]={0}; 
+    std::string bufString;
+    int recvLen, strLen; //수신메시지 길이, 문자열 길이
+
+    while(recv(clnt.sock,buffer,BUF_SIZE,0)>0)
+    {
+        //메시지길이+메시지내용 전달 받음
+        bufString=buffer;
+        recvLen=bufString.size(); //수신된 문자열 길이 저장
+        //수신될 문자열 길이 저장
+        strLen=atoi(buffer)+bufString.substr(0,bufString.find(':')).size();
+        while(recvLen!=strLen) //길이가 일치하지 않으면 더 읽기
+        {
+            memset(buffer,0,BUF_SIZE); //버퍼 초기화
+            if(recv(clnt.sock,buffer,BUF_SIZE,0)<=0) ReceiveError(); //수신오류나면 스레드 종료
+            bufString.append(buffer); recvLen=bufString.size();
+        }
+        std::cout<<"전달된 메시지: "<<bufString<<std::endl; //임시
+        std::string msg=split(bufString,':')[1];
+        if(msg=="friend")
+        {
+            //친구 목록 출력
+            PrintFriendList(split(bufString,':'));
+        }
+        else if(msg=="add")
+        {
+            //친구 추가 결과
+            if(!strncmp(split(bufString,':')[2].c_str(),"Failed",7))
+            {
+                std::cout<<"존재하지 않는 계정이거나 이미 추가된 계정입니다. \n";
+            }
+            else if(!strncmp(split(bufString,':')[2].c_str(),"Success",8))
+            {
+                std::cout<<"성공적으로 추가되었습니다. \n";
+            }
+        }
+        else if(msg=="find")
+        {
+            //친구 찾기 결과
+            PrintFoundFriend(split(bufString,':'));
+        }
+
+        memset(buffer,0,BUF_SIZE); //버퍼 초기화
+    }
+    //수신 오류 나면 스레드 종료될것 이기에
+    ReceiveError();
+}
 //서버 접속(로그인) 프로세스
-void LoginProcess(ClientBase& clnt)
+bool LoginProcess()
 {
     //버퍼로 사용할 문자열
     char buffer[BUF_SIZE]={0}; 
@@ -64,8 +186,9 @@ void LoginProcess(ClientBase& clnt)
     itoa(nickName.size(),buffer,10);
     msg=buffer+nickName;
     send(clnt.sock,msg.c_str(),msg.size(),0); //서버에 전송
+    nickName.erase(nickName.find(':'),6); //닉네임 저장
     //서버로부터 결과 수신
-    recv(clnt.sock,buffer,BUF_SIZE,0);
+    if(recv(clnt.sock,buffer,BUF_SIZE,0)<0) ReceiveError();
     if(!strncmp(buffer,"Existed",8))
     {
         std::cout<<"==================================================\n"
@@ -78,13 +201,14 @@ void LoginProcess(ClientBase& clnt)
             {
                 //친구 정보 수신을 위한 서버에 데이터 요청
                 //문제점 서버에 닉네임-소켓 연결이 안됨 우선 서버에 연결 요청해야함
-                RequestFriendList(clnt);
+                // RequestFriendList(clnt);
+                return true;
                 break;
             }
             else if(answer=="n"||answer=="N")
             {
                 //닉네임 다시 입력
-                LoginProcess(clnt);
+                return false;
                 break;
             }
             else
@@ -93,46 +217,100 @@ void LoginProcess(ClientBase& clnt)
             }   
         }
     }
-    else
+    else if(!strncmp(buffer,"Success",8))
     {
         //닉네임 등록 및 접속 완료되면 친구 정보 요청
-        RequestFriendList(clnt);
+        // RequestFriendList(clnt);
+        return true;
     }
+    return false;
 }
-//친구 정보 요청 및 목록 출력
-void RequestFriendList(ClientBase& clnt)
+//친구 정보 요청
+void RequestFriendList()
 {
     //버퍼로 사용할 문자열
     char buffer[BUF_SIZE]={0}; 
     std::string msg;
-    int recvLen, strLen;
 
     //친구 정보 요청
-    msg=":FriendList:"+nickName.erase(nickName.find(':'),6);
+    msg=":FriendList:"+nickName;
     itoa(msg.size(),buffer,10);
     msg=buffer+msg;
     send(clnt.sock,msg.c_str(),msg.size(),0);
-    //친구 정보 수신해야함
-    memset(buffer,0,BUF_SIZE);
-    recv(clnt.sock,buffer,BUF_SIZE,0);
-    //길수도 있어서 길이 함께 전달하기로 함
-    msg=buffer;
-    recvLen=msg.size(); //수신된 문자열 길이 저장
-    //수신될 문자열 길이 저장
-    strLen=atoi(buffer)+msg.substr(0,msg.find(':')).size();
-    while(recvLen!=strLen)
-    {
-        memset(buffer,0,BUF_SIZE);
-        if(recv(clnt.sock,buffer,BUF_SIZE,0)<=0) return; //수신오류(서버 종료등의 이유)
-        msg.append(buffer); recvLen=msg.size();
-    }
-    std::cout<<split(msg,':')[1]<<std::endl; //수신된 총 메시지
-    if(split(msg,':')[1]=="none") //친구 목록이 없으면
+}
+//친구 목록 출력
+void PrintFriendList(std::vector<std::string> flist)
+{
+    system("cls");
+    std::cout<<"==================================================\n";
+    if(flist[2]=="none")
     {
         std::cout<<"아직 친구가 없습니다. \n";
     }
     else
     {
         //친구 닉네임 접속상태 목록형식으로 출력할 것
+        friendNum=atoi(flist[2].c_str()); //친구 수 저장
+        for(int i=0;i<friendNum*2;i+=2)
+        {
+            if(posforF==i/2) std::cout<<"\x1b[33m"+flist[3+i]+"\x1b[m\t";
+            else std::cout<<flist[3+i]+"\t";
+            if(flist[4+i]=="F")
+            {
+                std::cout<<"\x1b[31m●\x1b[m\n";
+            }
+            else
+            {
+                std::cout<<"\x1b[32m●\x1b[m\n";
+            }
+        }
     }
+    std::cout<<"==================================================\n";
+    if(posforF==0+friendNum) std::cout<<"\x1b[33m친구 찾기 > \x1b[m\n";
+    else std::cout<<"친구 찾기 > \n";
+    if(posforF==1+friendNum) std::cout<<"\x1b[33m친구 추가 > \x1b[m\n";
+    else std::cout<<"친구 추가 > \n";
+}
+//친구 추가
+void AddorFindMyFriend(std::string AorF)
+{ //--닉네임 입력-서버 송신-수신-친구 목록 업데이트
+    //버퍼로 사용할 문자열
+    char buffer[BUF_SIZE]={0}; 
+    std::string msg;
+
+    std::string friendNick;
+    std::cout<<"친구의 닉네임을 입력하세요 >";
+    std::getline(std::cin,friendNick);
+    while(friendNick==nickName) //본인 닉네임 입력했을경우...이런것까지 생각해야해?
+    {
+        std::cout<<"본인 닉네임입니다. 다시 입력하세요 >";
+        std::getline(std::cin,friendNick);
+    }
+    msg=":"+AorF+":"+friendNick;
+    itoa(msg.size(),buffer,10);
+    msg=buffer+msg;
+    send(clnt.sock,msg.c_str(),msg.size(),0);
+}
+//찾은 친구 목록 출력
+void PrintFoundFriend(std::vector<std::string> flist)
+{
+    std::cout<<"==================================================\n";
+    if(flist[2]=="none")
+    {
+        std::cout<<"해당하는 친구가 없습니다. \n";
+        return;
+    }
+    for(int i=0;i<atoi(flist[2].c_str())*2;i+=2)
+    {
+        std::cout<<flist[3+i]<<"\t";
+        if(flist[4+i]=="F")
+        {
+            std::cout<<"\x1b[31m●\x1b[m\n";
+        }
+        else
+        {
+            std::cout<<"\x1b[32m●\x1b[m\n";
+        }
+    }
+    
 }
